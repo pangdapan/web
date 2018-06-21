72258ad796f65ea4cb526890e38015d975584623
guopan <panguo0109@qq.com>
2018-06-21 12:42:45 +0800

    web project Restaurant

---
+++ Restaurant/src/main/java/com/netease/NoodlesServlet.java
@@ -0,0 +1,24 @@
+package com.netease;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
+
+
+public class NoodlesServlet extends HttpServlet{
+    private static Logger log = Logger.getLogger(NoodlesServlet.class.getClass());
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+                                PrintWriter writer=response.getWriter();
+		String vegetable=request.getParameter("vegetable");
+		if(vegetable==null){
+			vegetable="Tamato";
+		}                              
+		writer.println("<html><body>");
+		writer.println("<h1>Noodles with  " +vegetable+"</h1>");
+		writer.println("</body></html>");
+        log.info("Noodles with "+vegetable);
+	}
+}
